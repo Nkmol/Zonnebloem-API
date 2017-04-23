12e29a6dd24f1900577237b79a97a15f0eb0ef5b
TobyWanKanobi <t.p.maipauw@gmail.com>
2017-04-23 22:19:05 +0200

    enum validation

--- components/obstacle/models/obstacle.model.js
+++ components/obstacle/models/obstacle.model.js
@@ -1,17 +1,30 @@
 'use strict';
 
-
-const GOOD = 'GOOD';
-const BAD = 'BAD';
-const IN_PORGRESS = 'IN_PROGRESS';
-const FIXED = 'FIXED';
-
 /**
  * Module dependencies
  */
 let mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     ObjectId = Schema.Types.ObjectId;
+
+// obstacle states
+const NEW = 'NEW';
+const REPORTED = 'REPORTED';
+const DECLINED = 'DECLINED';
+const IN_PORGRESS = 'IN_PROGRESS';
+const SOLVED = 'SOLVED';
+
+// obstacle severity states
+const HIGH = 'HIGH';
+const MEDIUM = 'MEDIUM';
+const LOW = 'LOW';
+
+let imageSchema = new Schema({
+    url: {
+        type: String,
+        required: true
+    }
+})
 
 let obstacleSchema = new Schema ({
 
@@ -32,18 +45,22 @@ let obstacleSchema = new Schema ({
     created_by: { 
         type: ObjectId, ref: 'User'
     },
-    images: [
-        { type: ObjectId, ref: 'Image' }
-    ],
+    images: [imageSchema],
     state: {
         type: String,
         enum: {
-            values: [GOOD, BAD, IN_PORGRESS, FIXED]
+            values: [NEW, REPORTED, DECLINED, IN_PORGRESS, SOLVED],
+            message: 'Invalid  obstacle state for `{PATH}` with value `{VALUE}`'
         },
         required: true
     },
     factor: {
-        type: ObjectId, ref: 'Factor'
+        type: String,
+        enum: {
+            values: [HIGH, MEDIUM, LOW],
+            message: 'Invalid  severity factor for `{PATH}` with value `{VALUE}`'
+        },
+        required: true
     }
 },
 {
