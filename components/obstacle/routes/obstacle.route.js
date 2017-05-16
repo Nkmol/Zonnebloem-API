79849a94b4ab7804c30ba4b648560998e9d1034f
Nkmol <Sandermol95@hotmail.com>
2017-05-16 21:08:38 +0200

    Obstacle routers as example

--- components/obstacle/routes/obstacle.route.js
+++ components/obstacle/routes/obstacle.route.js
@@ -2,6 +2,12 @@ let router = require("express").Router();
 let controller = require("../controllers/obstacle.controller");
 
 router.route("/")
-    .get(controller.get);
+    .get(controller.get)
+    .post(controller.create);
+
+router.route("/:_id")
+    .delete(controller.delete)
+    .put(controller.put)
+    .patch(controller.patch);
 
 module.exports = router;
