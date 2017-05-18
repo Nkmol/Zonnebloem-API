cb439f44ea0a15c4590fb089451ad03493068406
Nkmol <Sandermol95@hotmail.com>
2017-05-18 17:27:27 +0200

    Make a department optional - For future proof, users can test without being assigned to a Department/Regio - A User has minimal 1 Role, which is "GUEST" (A user that is not directly associated with Zonnebloem)

--- components/user/models/user.model.js
+++ components/user/models/user.model.js
@@ -8,15 +8,17 @@ let addressSchema = require("../../shared/address.schema");
 let bcrypt = require("bcrypt-nodejs");
 
 // User roles
-let ROLES = [ "ADMIN", "MODERATOR", "CONTROLER", "VOLUNTEER" ];
+let ROLES = [ "ADMIN", "MODERATOR", "CONTROLER", "VOLUNTEER", "GUEST" ];
 
 let userRoleSchema = new Schema({
     "role": {
         "type": String,
         "enum": {
             "values": ROLES,
+            "default": "GUEST",
             "message": "`{VALUE}` is not a valid user role."
-        }
+        },
+        "required": true
     },
     "department": {
         "type": ObjectId,
@@ -67,6 +69,25 @@ let userSchema = new Schema({
             "updatedAt": "updated_at"
         }
     });
+   
+// Load default user role if non are specified
+userSchema.pre("save", false, function(next) {
+    // If a department (role object) has been defined without a Role
+    if (this.roles && this.roles.length > 0) {
+        this.roles.forEach((userRole, i) => {
+            if (!userRole.role) {
+                this.user_role[ i ].role = "VOLUNTEER";
+            }
+        });
+    }
+    // When no department has been given, we assume this is a guest (not directly working with Zonnebloem)
+    else if (!this.roles || this.roles.length <= 0) {
+        this.roles = [];
+        this.roles.push({ "role": "GUEST" });
+    }
+
+    next();
+});
 
 userSchema.statics.generateHash = function(password) {
     return new Promise((resolve, reject) => {
