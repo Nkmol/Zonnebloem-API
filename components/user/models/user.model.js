ac7fa348cf914e7cc20c40507bdfc4efce7f9553
Nkmol <Sandermol95@hotmail.com>
2017-05-18 17:37:01 +0200

    Make updated ApiDoc of User structure

--- components/user/models/user.model.js
+++ components/user/models/user.model.js
@@ -70,18 +70,10 @@ let userSchema = new Schema({
         }
     });
    
-// Load default user role if non are specified
+// Load default user role if none are specified
 userSchema.pre("save", false, function(next) {
-    // If a department (role object) has been defined without a Role
-    if (this.roles && this.roles.length > 0) {
-        this.roles.forEach((userRole, i) => {
-            if (!userRole.role) {
-                this.user_role[ i ].role = "VOLUNTEER";
-            }
-        });
-    }
     // When no department has been given, we assume this is a guest (not directly working with Zonnebloem)
-    else if (!this.roles || this.roles.length <= 0) {
+    if (!this.roles && this.roles.length <= 0) {
         this.roles = [];
         this.roles.push({ "role": "GUEST" });
     }
