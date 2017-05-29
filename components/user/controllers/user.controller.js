37d1dd8d1e81ffb0b4c9f7312a1f7108bd5d7c8f
Sander Mol <Sandermol95@hotmail.com>
2017-05-29 14:08:52 +0200

    Fix put (#64)
    * Feature/basic crud (#33)
    
    * - Move the authentication code from the UserController to LoginController
    - Implement get and getOne for users
    
    * Exclude password for user model
    
    * return 404 statusCode if user is not found
    
    * create the create, update, delete routes
    
    * Fix password exclusion bug
    
    * Remove paging and add create function
    
    * Feature/crud (#37)
    
    * Simplyfied error handler + create
    - Add create on base.controller
    - Simplified MongooseError handleing
    
    * Add PUT method
    - change geodata to standard of http://geojson.org/
    - add base method of PUT
    
    * Add base remove method
    
    * Add PATCH request
    - POST now also always give saved document back
    - PUT now also return `_id` with the data
    
    * Obstacle routers as example
    
    * PUT now replaces the document
    
    * Implement unit testing for users
    
    * Feature/apidoc (#42)
    
    * Simplyfied error handler + create
    - Add create on base.controller
    - Simplified MongooseError handleing
    
    * Add PUT method
    - change geodata to standard of http://geojson.org/
    - add base method of PUT
    
    * Add base remove method
    
    * Add PATCH request
    - POST now also always give saved document back
    - PUT now also return `_id` with the data
    
    * Obstacle routers as example
    
    * PUT now replaces the document
    
    * Define apidoc
    
    * Define logic docs
    
    * Define user docs
    
    * Define obstacle docs
    
    * Init generated ApiDoc
    
    * Update the npm test command in package.json
    
    * Fix/user (#45)
    
    * Simplyfied error handler + create
    - Add create on base.controller
    - Simplified MongooseError handleing
    
    * Add PUT method
    - change geodata to standard of http://geojson.org/
    - add base method of PUT
    
    * Add base remove method
    
    * Add PATCH request
    - POST now also always give saved document back
    - PUT now also return `_id` with the data
    
    * Obstacle routers as example
    
    * PUT now replaces the document
    
    * Define apidoc
    
    * Define logic docs
    
    * Define user docs
    
    * Define obstacle docs
    
    * Init generated ApiDoc
    
    * Make a department optional
    - For future proof, users can test without being assigned to a Department/Regio
    - A User has minimal 1 Role, which is "GUEST" (A user that is not directly associated with Zonnebloem)
    
    * Make updated ApiDoc of User structure
    
    * Feature/routes (#49)
    
    * Implement departments routes + unit tests + docs
    
    * Add report routes
    
    * Add routes routes
    
    * Add unit tests for report
    
    * Add unit tests for /routes
    
    * Add unit tests for /obstacles
    
    * add a before script to travis.yml
    
    * update travis
    
    * Feature/apidoc (#50)
    
    * Complete apidoc
    
    * Fix login information
    - Able to post a whole User{ } as post in `/register`
    
    * Fix tests
    - updated apidoc fix
    
    * Feature/upload (#52)
    
    * Add basic upload functionality
    - Add filestack service to upload files
    - Add route with `multipart` to upload files
    
    * Always remove temp file
    
    * Add remove file method
    
    * Apidoc of file routers
    
    * Multiple files support
    
    * Improve error messages of bulk remove files
    - One request can now fail, without canceling other request (allows for one fail, one success)
    - Responses are given as an array of Response Objects
    
    * Same structure as remove file
    - One file post request can fail, while the other succeeds
    - Now returns multiple responses and errors accordinly
    
    * Move the fileStack response as message instead of data
    
    * Update apiDoc
    
    * Update apidoc
    
    * Fix put

--- components/user/controllers/user.controller.js
+++ components/user/controllers/user.controller.js
@@ -7,66 +7,7 @@ class UserController extends BaseController {
         super();
         this._model = User;
     }
-
-    // get(req, res, next) {
-    //     super.get(req, res, next);
-        
-
-        // Implement this in a later phase
-        // let limit = req.query.limit || 10;
-        // let page = req.query.page || 1;
-        // let sort = req.query.sort || 'asc';
-        // let sortBy = req.query.sort_by || 'created_at';
-
-        // limit = parseInt(limit);
-        // page = parseInt(page);
-
-        // return this._model.count({})
-        // .then(itemsCount => this.fetchUsers(itemsCount, limit, page, sort, sortBy))
-        // .then(response => {
-
-        //     console.log('response', response);
-
-        //     if (!response.users) {
-        //         return res.status(404).json(this._combineStatus({
-        //             status: res.statusCode,
-        //             message: "No users found"
-        //         }));
-        //     }
-        //     return res.json(this._combineStatus({
-        //         data: {
-        //             total_count: response.itemsCount,
-        //             users: response.users
-        //         }
-        //     }));
-        // })
-        // .catch(e => {
-        //     console.log(e);
-        // })
-    // }
-
-    // fetchUsers(itemsCount, limit, page, sort, sortBy) {
-    //     let sortObj = {};
-    //     sortObj[sortBy] = sort;
-
-    //     return this._model.find({})
-    //         .skip((page - 1) * limit)
-    //         .limit(limit)
-    //         .sort(sortObj)
-    //         .then(users => {
-    //             console.log('users', users);
-    //             return {itemsCount, users}
-    //         })
-    // }
-
-    put(req, res, next) {
-        // Exclude fields from update
-        delete req.body.username;
-        delete req.body.password;
-
-        super.put(req, res, next);
-    }
-
+    
     me(req, res, next) {
         let user = req.user;
         if (user) {
