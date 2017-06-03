a6d512980d7a4a206d390a513009a0bbfcc69370
mohwaili <mohammed.alwaili@gmail.com>
2017-06-03 11:28:33 +0200

    implement roles for departments

--- components/department/routes/department.route.js
+++ components/department/routes/department.route.js
@@ -1,5 +1,23 @@
 let router = require("express").Router();
 let controller = require("../controllers/department.controller");
+let roles = require("../../config/roles.config");
+
+router.use(roles.middleware());
+
+// Only the moderator is allowed to modify the department
+roles.use('adjust department', (req) => {
+    let departmentId = req.params._id;
+    let me = req.user;
+    if ((departmentId && me) && (me.roles && me.roles.length > 0)) {
+        let isModerator = false;
+        me.roles.forEach(function(myRole) {
+            if (myRole.role == "MODERATOR" && myRole.department._id.equals(departmentId)) {
+                isModerator = true;
+            }
+        });
+        if (isModerator) return true;
+    }
+})
 
 router.route("/")
     .get(controller.get)
@@ -7,8 +25,8 @@ router.route("/")
 
 router.route("/:_id")
     .get(controller.getOne)
-    .put(controller.put)
-    .patch(controller.patch)
-    .delete(controller.delete);
+    .put(roles.can('adjust department'), controller.put)
+    .patch(roles.can('adjust department'), controller.patch)
+    .delete(roles.can('adjust department'), controller.delete);
 
 module.exports = router;
