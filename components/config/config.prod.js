0de5649d4957d469d85a9face71b8ef5a17ce08b
mohwaili <mohammed.alwaili@gmail.com>
2017-06-17 10:04:59 +0200

    Add mongoose connection options

--- components/config/config.prod.js
+++ components/config/config.prod.js
@@ -6,7 +6,8 @@ config.db.user = "user";
 config.db.password = "user";
 config.db.uri = `mongodb://${config.db.user}:${config.db.password}@ds062059.mlab.com:62059/zonnebloem`;
 config.db.options = {
-    
+    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }
 };
 
 module.exports = config;
